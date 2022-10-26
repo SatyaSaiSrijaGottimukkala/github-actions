2f5e055fb7421a120db7400827adff613c646ee8
SatyaSaiSrijaGottimukkala <a845604@yara.com>
2022-10-26 15:03:59 +0530

    trying new workflow

--- check.sh
+++ check.sh
@@ -1,17 +1,34 @@
-arr=()
-arr2=()
-for i in $(ls -d DataFolder/*/); do arr+=($i); done
-for value in "${arr[@]}"
-do
-     # echo $value
-     # for i in $(find $value -maxdepth 1 -type d); do arr2+=($i); done
-     for i in $(ls -d $value*/); do arr2+=($i); done
-done
+# arr=()
+# arr2=()
+# for i in $(ls -d DataFolder/*/); do arr+=($i); done
+# for value in "${arr[@]}"
+# do
+#      # echo $value
+#      # for i in $(find $value -maxdepth 1 -type d); do arr2+=($i); done
+#      for i in $(ls -d $value*/); do arr2+=($i); done
+# done
 
-for data in "${arr2[@]}"
+# for data in "${arr2[@]}"
+# do 
+#      echo $data
+# done
+
+
+# list_apac=$(git diff master trialbranch --name-only | grep DataFolder/apac-stage)
+
+
+arr3=()
+list_apac="none"
+for i in $(ls -d DataFolder/apac-stage/*/); do arr3+=($i); done
+for value in "${arr3[@]}"
 do 
-     echo $data
+     # echo $value
+     list_apac=$(git diff master trialbranch --name-only | grep $value)
+     echo $list_apac
+     if [[ $list_apac != "" ]]
+     then
+          echo "happens"
+     fi
+
+     
 done
-
-
-list_apac=$(git diff master trialbranch --name-only | grep DataFolder/apac-stage)
